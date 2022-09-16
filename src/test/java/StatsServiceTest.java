4a1a85f526694972c122fea44afce71370057ff6
ilonaesaulenko <ilonaesaulenko@yandex.ru>
2022-09-16 15:48:51 +0300

    Initial commit

---
+++ src/test/java/StatsServiceTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+
+    @Test
+    public void shouldSum() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+
+
+    }
+
